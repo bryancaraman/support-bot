1bdf6747a27e8e5e7fb887832821b817b18c49c3
Bryan Caraman <112526577+bryancaraman@users.noreply.github.com>
2024-08-09 11:04:04 -0400

    removed old code

--- app/api/chat/route.js
+++ app/api/chat/route.js
@@ -1,25 +1,6 @@
 import { NextResponse } from "next/server";
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// export async function POST(req) {
-//     try {
-//         const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-//         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-        
-//         const body = await req.json();
-//         const messages = body.map(msg => msg.content).join(' ');
-
-//         const result = await model.generateContent(messages);
-//         const response = await result.response;
-//         const text = await response.text(); 
-
-//         return NextResponse.json({ content: text }); 
-//     } catch (error) {
-//         console.error('Error in API route:', error);
-//         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-//     }
-// }
-
 export async function POST(req) {
   try {
     const { systemPrompt, messages } = await req.json(); // Expect JSON body
